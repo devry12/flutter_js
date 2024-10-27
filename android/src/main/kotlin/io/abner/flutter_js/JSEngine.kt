936e54e71167c3f62a3281991345e423d2c1925d
Devry Kawiryan <devrykawiryan@gmail.com>
2024-10-28 00:01:36 +0700

    Update JSEngine.kt

--- android/src/main/kotlin/io/abner/flutter_js/JSEngine.kt
+++ android/src/main/kotlin/io/abner/flutter_js/JSEngine.kt
@@ -1,82 +1,65 @@
 package io.abner.flutter_js
 
+import android.content.Context
 import android.util.Log
-//import de.prosiebensat1digital.oasisjsbridge.*
-//import kotlinx.coroutines.Dispatchers
 import java.util.*
-import java.util.logging.Logger
 
+class JSEngine(context: Context) {
 
-class JSEngine(context: android.content.Context) {
+    private var runtime: JsBridge = JsBridge(JsBridgeConfig.standardConfig())
+    private var messageChannelMap = mutableMapOf<String, (message: String) -> String>()
 
-    // private var runtime: JsBridge = JsBridge(JsBridgeConfig.standardConfig())
-    // private var messageChannelMap = mutableMapOf<String, (message: String) -> String>()
+    init {
+        val errorListener = object : JsBridge.ErrorListener() {
+            override fun onError(error: JsBridgeError) {
+                Log.e("JSEngine", error.errorString())
+            }
+        }
+        runtime.registerErrorListener(errorListener)
 
-    // fun getRuntime(): JsBridge {
-    //     return runtime
-    // } 
+        // Register a UUID function in JS
+        JsValue.fromNativeFunction0(runtime) {
+            UUID.randomUUID().toString()
+        }.assignToGlobal("FLUTTERJS_getUUID")
 
-    // var runtimeInitialized = false
-    // val host = "localhost"
+        // Register a message sending function in JS
+        JsValue.fromNativeFunction2(runtime) { channelName: String, message: String ->
+            try {
+                messageChannelMap[channelName]?.invoke(message)
+                "$channelName:$message"
+            } catch (e: Exception) {
+                e.message ?: "Error"
+            }
+        }.assignToGlobal("FLUTTERJS_sendMessage")
 
-    // val port = 0
-    // init {
+        // Initialize pending messages in JS
+        runtime.evaluateBlocking(
+            """
+            var FLUTTERJS_pendingMessages = {};
+            function sendMessage(channel, message) {
+                var idMessage = FLUTTERJS_getUUID();
+                return new Promise((resolve, reject) => {
+                    FLUTTERJS_pendingMessages[idMessage] = { 
+                        resolve: (v) => { resolve(v); return v;}, 
+                        reject: reject
+                    };
+                    FLUTTERJS_sendMessage(channel, JSON.stringify({ id: idMessage, message: message }));
+                });
+            }
+            """.trimIndent(),
+            JsonObjectWrapper::class.java
+        )
+    }
 
-    //     val errorListener = object : JsBridge.ErrorListener(Dispatchers.Main) {
-    //         override fun onError(error: JsBridgeError) {
-    //             Log.e("MainActivity", error.errorString())
-    //         }
-    //     }
-    //     runtime.registerErrorListener(errorListener)
+    fun registerChannel(channelName: String, channelFn: (message: String) -> String) {
+        messageChannelMap[channelName] = channelFn
+    }
 
-    //     val getUUID = JsValue.fromNativeFunction0(runtime) {
-    //         UUID.randomUUID().toString()
-    //     }.assignToGlobal("FLUTTERJS_getUUID")
+    fun eval(script: String): JsonObjectWrapper {
+        return runtime.evaluateBlocking(script, JsonObjectWrapper::class.java) as JsonObjectWrapper
+    }
 
-    //     val sendMessage = JsValue.fromNativeFunction2(runtime) { channelName: String, message: String ->
-
-    //         try {
-    //             if (messageChannelMap.containsKey(channelName)) {
-    //                 messageChannelMap.getValue(channelName).invoke(message);
-    //             } else {
-    //                 Log.i("SendMessage Channel", "Channel ${channelName} wasn't registered!")
-    //             }
-
-    //             return@fromNativeFunction2 "$channelName:$message"
-    //         } catch (e: Exception) {
-    //             return@fromNativeFunction2 e.message
-    //         }
-            
-    //     }.assignToGlobal("FLUTTERJS_sendMessage")
-
-    //     runtime.evaluateBlocking(
-    //             """
-    //                 var FLUTTERJS_pendingMessages = {};
-    //                 function sendMessage(channel, message) {
-    //                     var idMessage = FLUTTERJS_getUUID();
-    //                     return new Promise((resolve, reject) => {
-    //                         FLUTTERJS_pendingMessages[idMessage] = { 
-    //                             resolve: (v) => { resolve(v); return v;}, 
-    //                             reject: reject
-    //                         };
-    //                         FLUTTERJS_sendMessage(channel, JSON.stringify({ id: idMessage, message: message }) );
-    //                     });
-    //                 }
-    //             """.trimIndent(),
-    //             JsonObjectWrapper::class.java
-    //     )
-    // }
-
-    // fun registerChannel(channelName: String, channelFn: (message: String) -> String) {
-    //     messageChannelMap[channelName] = channelFn
-    // }
-
-    // fun eval(script: String): JsonObjectWrapper {
-    //     return runtime.evaluateBlocking(script, JsonObjectWrapper::class.java) as JsonObjectWrapper
-    // }
-
-    // fun release() {
-    //     runtime.release()
-    // }
-
+    fun release() {
+        runtime.release()
+    }
 }
